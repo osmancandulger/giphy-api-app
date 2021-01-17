6a53b97cd3586c5897167fd7a2fece764a01842f
osmancandulger <osmancandulger@gmail.com>
2021-01-18 02:01:23 +0300

    Added Search component and styled

---
+++ src/components/Search.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search-bar">
+    <input type="text" placeholder="Put some words in here !" />
+    <button>Find My Mood</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.search-bar {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin: 35px;
+  align-items: center;
+  input {
+    width: 350px;
+    height: 45px;
+    border-radius: 15px;
+    outline: none;
+    border: none;
+    background: #bfdad3;
+  }
+  input::placeholder {
+    color: rgb(99, 92, 92);
+    padding-left: 10px;
+    opacity: 0.7;
+  }
+  button {
+    position: relative;
+    left: 10px;
+    width: 150px;
+    height: 45px;
+    border: none;
+    border-radius: 15px;
+    outline: none;
+    cursor: pointer;
+    
+  }
+}
+</style>
