e3afcbe2e25e02611c010d44d4edd5cb97daed19
osmancandulger <osmancandulger@gmail.com>
2021-01-18 15:25:54 +0300

    Added gifs into Overwiew page and Added style

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
-<template> <search></search></template>
+<template>
+  <search></search>
+  <overview :gifs="gifs"></overview>
+</template>
 
 <script>
 import Search from "./components/Search.vue";
+import Overview from "./components/Overview.vue";
 
 export default {
   name: "App",
   components: {
     Search,
+    Overview,
+  },
+  data() {
+    return {
+      gifs: [],
+    };
+  },
+  created() {
+    fetch("http://api.giphy.com/v1/stickers/trending?api_key=dc6zaTOxFJmzC")
+      .then((response) => response.json())
+      .then((response) => (this.gifs = response.data));
   },
 };
 </script>
--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -2,7 +2,9 @@
   <div class="gifs">
     <ul>
       <li v-for="item in gifs" :key="item">
-        test
+        <a :href="item.embed_url" target="_blank">
+          <img :src="item.images.fixed_height.url"
+        /></a>
       </li>
     </ul>
   </div>
@@ -10,13 +12,28 @@
 
 <script>
 export default {
-    props:['gifs']
-  data() {
-    return {
-
-    };
-  },
+  props: ["gifs"],
 };
 </script>
 
-<style></style>
+<style>
+.gifs {
+  display: flex;
+  justify-content: center;
+}
+ul {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 75px;
+}
+li {
+  list-style: none;
+  transition: 0.4s all;
+}
+li:hover {
+  transform: scale(1.1) rotate(-15deg);
+}
+</style>
